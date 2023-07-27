92a4a5c3445c0e75bb68f85beffd0599bc08f450
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-07-27 19:41:22 +0200

    ReturnBook init

--- src/main/java/cz/cuni/mff/java/zapoctak/content/ReturnBook.java
+++ src/main/java/cz/cuni/mff/java/zapoctak/content/ReturnBook.java
@@ -1,11 +1,53 @@
 package cz.cuni.mff.java.zapoctak.content;
 
+import cz.cuni.mff.java.zapoctak.global.TitleBorder;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class ReturnBook extends JPanel {
 
+    private JTextField documentID;
     public ReturnBook(){
-        System.out.println("return book");
+        this.setBorder(TitleBorder.create("Vrácení knihy"));
+        documentID = new JTextField(11);
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = getGridBagConstraints();
+
+        addDocumentIDLabelAndField(gbc);
+    }
+
+    private void addDocumentIDLabelAndField(GridBagConstraints gbc) {
+        JLabel documentIDLabel = new JLabel("ID objednávky:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        add(documentIDLabel, gbc);
+
+        gbc.gridx = 1;
+        add(documentID, gbc);
+
+        gbc.gridx = 2;
+        JButton findButton = new JButton("Hledat");
+        findButton.addActionListener(e -> {
+            showDocument();
+        });
+        add(findButton, gbc);
+    }
+
+    private void showDocument() {
+        
+    }
+
+
+    private GridBagConstraints getGridBagConstraints() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        return gbc;
     }
 
 }
