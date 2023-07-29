96b97a402a9478437775089c828abed08c4f8bf5
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-07-29 14:52:18 +0200

    AddAuthor update

--- src/main/java/cz/cuni/mff/java/zapoctak/content/AddAuthor.java
+++ src/main/java/cz/cuni/mff/java/zapoctak/content/AddAuthor.java
@@ -15,91 +15,88 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.TreeSet;
 
-
-/**
- * This class represents a panel for adding a new author.
- *
- * @author Duong Xuan Anh
- * @version 1.0
- */
-
 public class AddAuthor extends JPanel {
-    /**
-     * A JTextField to take the name of the author.
-     */
-    private JTextField nameField;
+    private final JTextField nameField;
+    private final JComboBox<String> nationalComboBox;
 
-    /**
-     * A JComboBox to select the nationality of the author.
-     */
-    private JComboBox<String> nationalComboBox;
-    public AddAuthor(){
+    public AddAuthor() {
         this.setBorder(TitleBorder.create("Přidat autora"));
+        nameField = new JTextField(20);
+        nationalComboBox = new JComboBox<>(getAvailableCountries());
+        setupLayout();
+        setupSubmitButton();
+    }
 
-        // Set the layout of the panel.
+    private void setupLayout() {
         setLayout(new GridBagLayout());
+        GridBagConstraints gbc = getGridBagConstraints();
+
+        addNameLabelAndField(gbc);
+        addNationalLabelAndComboBox(gbc);
+    }
+
+    private GridBagConstraints getGridBagConstraints() {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
+        return gbc;
+    }
 
+    private void addNameLabelAndField(GridBagConstraints gbc) {
         JLabel nameLabel = new JLabel("Jméno Autora");
-        nameField = new JTextField(20);
-
-        JLabel nationalLabel = new JLabel("Národnost");
-
-        Locale[] locales = Locale.getAvailableLocales();
-        Set<String> countries = new TreeSet<String>();
-        for (Locale locale : locales) {
-            countries.add(locale.getDisplayCountry());
-        }
-
-        nationalComboBox = new JComboBox<>(countries.toArray(new String[0]));
-
-        JButton submitButton = new JButton("Přidat autora");
-        submitButton.addActionListener(this::addAuthorAction);
-
         gbc.gridx = 0;
         gbc.gridy = 0;
         add(nameLabel, gbc);
 
         gbc.gridx = 1;
         add(nameField, gbc);
+    }
 
+    private void addNationalLabelAndComboBox(GridBagConstraints gbc) {
+        JLabel nationalLabel = new JLabel("Národnost");
         gbc.gridx = 0;
         gbc.gridy = 1;
         add(nationalLabel, gbc);
 
         gbc.gridx = 1;
         add(nationalComboBox, gbc);
+    }
 
+    private void setupSubmitButton() {
+        JButton submitButton = new JButton("Přidat autora");
+        GridBagConstraints gbc = getGridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
         add(submitButton, gbc);
+
+        submitButton.addActionListener(this::addAuthorAction);
     }
 
-
-    /**
-     * The addAuthorAction method is called when the submit button is clicked.
-     * It checks if the name field is empty and if not, adds the author to the database.
-     *
-     * @param e The ActionEvent object.
-     */
     private void addAuthorAction(ActionEvent e) {
         String name = nameField.getText();
         String national = (String) nationalComboBox.getSelectedItem();
 
+        if (checkAuthorData(name, national)) {
+            insertAuthorToDb(name, national);
+        }
+    }
+
+    private boolean checkAuthorData(String name, String national) {
         if (name.isEmpty()) {
             Notification.showErrorMessage("Jméno autora nesmí být prázdné");
-            return;
+            return false;
         }
 
         if (national == null || national.isEmpty()) {
             Notification.showErrorMessage("Národnost autora musí být vybrána");
-            return;
+            return false;
         }
+        return true;
+    }
 
+    private void insertAuthorToDb(String name, String national) {
         try (Connection conn = Config.getConnection();
              PreparedStatement checkStatement = conn.prepareStatement("SELECT id, jmeno FROM autor WHERE jmeno = ?");
              PreparedStatement insertStatement = conn.prepareStatement("INSERT INTO autor (jmeno, narodnost) VALUES (?, ?)")) {
@@ -122,10 +119,16 @@ public class AddAuthor extends JPanel {
             Notification.showErrorMessage("Nastal problém, zkuste to znovu nebo informujte IT oddělení");
         }
     }
-    
-    /**
-     * This method resets the form.
-     */
+
+    private String[] getAvailableCountries() {
+        Locale[] locales = Locale.getAvailableLocales();
+        Set<String> countries = new TreeSet<>();
+        for (Locale locale : locales) {
+            countries.add(locale.getDisplayCountry());
+        }
+        return countries.toArray(new String[0]);
+    }
+
     private void resetForm() {
         nameField.setText("");
         nationalComboBox.setSelectedIndex(0);
